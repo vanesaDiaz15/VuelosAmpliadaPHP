50eb8dc68e5cd449412865882dc71e2348c9865a
Vanesa <vanesadiaz15mayo@gmail.com>
2021-01-20 12:38:40 +0100

    Insertar

---
+++ apiPOST.php
@@ -0,0 +1,65 @@
+<?php
+function insertarUNO($_DATA, $coleccion)
+{
+    $arrayMensaje = array(); //Asociativo
+    $vendido = array(
+        "dni" => $_DATA['dni'],
+        "apellido" => $_DATA['apellido'],
+        "nombre" => $_DATA['nombre'],
+        "dniPagador" => $_DATA['dniPagador'],
+        "tarjeta" => $_DATA['tarjeta']
+    );
+    try {
+        $filterOption = array("codigo" => $_DATA['codigo']);
+        $datos = array("vendidos" => $vendido);
+
+        $coleccion->updateMany(
+            $filterOption,
+            ['$push' => $datos, '$inc' => array("plazas_disponibles" => -1)]
+        );
+
+        $vuelo = $coleccion->find(array("codigo" => $_DATA['codigo']));
+        $fecha = "";
+        $destino = "";
+        $origen = "";
+        $hora = "";
+        $precio = "";
+
+        foreach ($vuelo as $entry) {
+            $origen = $entry['origen'];
+            $destino = $entry['destino'];
+            $fecha = $entry['fecha'];
+            $hora = $entry['hora'];
+            $precio = $entry['precio'];
+        }
+
+
+        $arrayMensaje[] = array(
+            "estado" => true,
+            "codigo" => $_DATA['codigo'],
+            "origen" => $origen,
+            "destino" => $destino,
+            "fecha" => $fecha,
+            "hora" => $hora,
+            "asiento" => 3,
+            "dni" => $_DATA['dni'],
+            "apellido" => $_DATA['apellido'],
+            "nombre" => $_DATA['nombre'],
+            "dniPagador" => $_DATA['dniPagador'],
+            "tarjeta" => $_DATA['tarjeta'],
+            "codigoVenta" => "GHJ7766GG",
+            "precio" => $precio,
+        );
+
+    } catch (Exception $e) {
+        $arrayMensaje[] = array(
+            "estado" => false,
+            "mensaje" => "No se ha podido realizar la compra por $e"
+        );
+    }
+
+    $mensajeJSON = json_encode($arrayMensaje, JSON_PRETTY_PRINT);
+    echo $mensajeJSON;
+}
+
+?>
